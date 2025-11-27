433e327550e1a508f6c47cc51b5d85ce93481319
chatchai <chsuriyawan@yahoo.com>
2025-11-27 20:40:55 +0700

    connect to google cloud service

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -45,7 +45,7 @@ const initializePassport = () => {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: `${process.env.SERVER_URL}/api/v1/auth/google/callback`,
+        callbackURL: `${process.env.GOOGLE_CALLBACK_URL}`,
       },
       (accessToken, refreshToken, profile, done) => {
         // This function is called after successful authentication with Google
